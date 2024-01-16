1ae5e232993e556b250d438d10c9150add41c2b1
usman4li <ali.usman@gmail.com>
2024-01-16 22:59:02 +0700

    ALT Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WeatherContainer from './Containers/WeatherContainer';
+import Layout from './Layout/Layout';
+
 
 
 function App () {
-  
+  const apiKey = '75d5132159c1a539c6a433e52d0094a1';
   return (
-    
+     
     <div className='app'>
-      <WeatherContainer/>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout/>}/>
+            <Route path='/' element={<WeatherContainer apiKey={apiKey}/>}/>
+            <Route path='*' element={<h1>404</h1>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/Containers/WeatherContainer/index.tsx
+++ src/Containers/WeatherContainer/index.tsx
@@ -1,63 +1,119 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Input } from '../../Components';
 
-const WeatherApp: React.FC = () => {
+interface WeatherAppProps {
+  apiKey: string;
+}
 
-  const [data, setData] = useState<any>({})
+interface WeatherData {
+  name?: string;
+  main?: {
+    temp?: number;
+    feels_like?: number;
+    humidity?: number;
+  };
+  weather?: [{ main: string }];
+  wind?: {
+    speed?: number;
+  };
+}
+
+const WeatherApp: React.FC<WeatherAppProps> = ({ apiKey }) => {
+
+  const [data, setData] = useState<WeatherData>({})
   const [location, setLocation] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=75d5132159c1a539c6a433e52d0094a1`
-  const searchLocation = (event: { key: string; }) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
+
+  const searchLocation = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      axios.get(url).then(
-        (response) => {
-          setData(response.data)
-          console.log(response.data)
-        }
-      )
+      fetchData();
     }
-  }
+  };
+  const fetchData = () => {
+    setLoading(true);
+    setError(null);
 
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        // console.log(response.data)
+      })
+      .catch((error) => {
+        setError('Error feching weather data. Please Enter Location');
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
+  };
+
+  useEffect(() => {
+    console.log('Fetching weather data...');
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('Fetching weather data...', data);
+  }, [data]);
+
+  useEffect(() => {
+    console.log('Hamburger component rendered');
+  }, []);
+
+  useEffect(() => {
+    const event = new CustomEvent('weatherDataLoaded', { detail: data });
+    window.dispatchEvent(event);
+  }, [data]);
   return (
     <div className='app'>
       <div className="search">
         <Input 
+          id='locationInput'
           value={location}
           onChange={event => setLocation(event.target.value)}
           onKeyPress={searchLocation}
           placeholder='Enter Location'
           type="text" />
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {data.name && (
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1 className='bold'>{data.main.temp}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null}
+            </div>
           </div>
-          <div className="temp">
-            {data.main ? <h1 className='bold'>{data.main.temp}°C</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p className='bold'>{data.weather[0].main}</p> : null}
-          </div>
+          {data.name != undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like}°C</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind? <p className='bold'>{data.wind.speed} Km/h</p> : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          }
         </div>
-        {data.name != undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like}°C</p> : null}
-              <p>Feels Like</p>
-            </div>
-            <div className="humidity">
-            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind? <p className='bold'>{data.wind.speed} Km/h</p> : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        }
-      </div>
+      )}
     </div>
   );
 };
