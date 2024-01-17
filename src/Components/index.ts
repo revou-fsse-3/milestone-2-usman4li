9222d27b8ad8cfcc1868884320c90c0dc68a58b8
usman4li <ali.usman@gmail.com>
2024-01-17 23:56:58 +0700

    update button gak bisa deploy

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './Button';
+export { default as Button } from './button';
 export { default as Input } from './Input';
 export { default as Navigasi } from './Navigasi';
